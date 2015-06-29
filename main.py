95d65a16161610523979317f9c909ef753e37dc3
nanselmo <nanselmo@nanselmo-macbookpro.roam.corp.google.com>
2015-06-29 12:14:29 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import os
+import jinja2
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+    	template = jinja_environment.get_template('templates/main.html')
+    	self.response.out.write(template.render())
+    def post(self):
+    	answer1 = self.request.get('stanley')
+    	answer2 = self.request.get('willis')
+    	user_answers = [answer1, answer2]
+    	answer_count = 0
+    	if answer1.lower() == "chicago":
+    		answer_count += 1
+    	if answer2.lower() == "sears":
+    		answer_count += 1
+    	template = jinja_environment.get_template('templates/results.html')
+    	self.response.out.write(template.render({'answers': user_answers, 'count': answer_count}))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
